cae42c7263476ee77d1fe20cbf68fbfe6217e38f
Logan Klein <lkleindesigns@gmail.com>
2017-08-05 16:07:42 -0500

    Add new message and create message events

--- server/server.js
+++ server/server.js
@@ -14,11 +14,20 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
   console.log('New user connected')
 
+  socket.emit('newMessage', {
+    from: 'Me on the server',
+    text: 'Emitting events from the server',
+    createdAt: Date.now()
+  })
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message)
+  })
+
   socket.on('disconnect', () => {
     console.log('User disconnected')
   })
 })
-
 
 
 server.listen(port, () => {
